00a10e5dbacfeafa3e61a20d94f9f5d975e06e9a
unknown <vitop.hieunguyen@gmail.com>
2018-06-01 22:10:31 +0700

    monhoc add

--- resources/assets/js/components/monhoc/MonhocEdit.vue
+++ resources/assets/js/components/monhoc/MonhocEdit.vue
@@ -1,24 +1,28 @@
 <template id="monhoc-edit">
-  <div class="row">
-    <h1>Đây là trang môn học</h1>
-  </div>
+    <div class="row">
+        <h1>Edit Page</h1>
+    </div>
 </template>
 
 <script>
-export default {
-  data:function(){
-    return {
+    export default {
+        data: function () {
+            return {
+                vail: false,
+                monhoc: {
+                    id: '',
+                    tenmonhoc: '',
+                    ngaybatdau: '',
+                    ngayketthuc: '',
+                    menubatdau: false,
+                    getMenuketthuc: false,
+                },
+            };
+        },
+        created() {
 
-    };
-  },
-  created() {
-
-  },
-  methods: {
-
-  },
-  computed: {
-
-  }
-}
+        },
+        methods: {},
+        computed: {}
+    }
 </script>
